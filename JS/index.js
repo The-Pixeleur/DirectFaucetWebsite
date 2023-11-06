0bacf60b48436e369838efea735e5b1a71266f51
Evan E <evan.escabasse@gmail.com>
2023-11-06 10:20:02 +0100

    Style Update
    => Modified background
         -Added an animated gradient background
    => Modified crypto logo and name
         -Added animation for appear and disappear
    => Modified roadmap
         -Added style for the roadmap

--- JS/index.js
+++ JS/index.js
@@ -1,8 +1,27 @@
-let logos = ["./IMG/CryptoLogo/ethereum.webp", "./IMG/CryptoLogo/bitcoin.webp", "./IMG/CryptoLogo/litecion.webp", "./IMG/CryptoLogo/dogecoin.webp", "./IMG/CryptoLogo/avax.webp", "./IMG/CryptoLogo/matic.webp", "./IMG/CryptoLogo/Bnb.webp", "./IMG/CryptoLogo/Vite.png", "./IMG/CryptoLogo/digibyte.webp", "./IMG/CryptoLogo/stellar.webp", "./IMG/CryptoLogo/solana.webp"];
-let cryptos = ["Ethereum", "Bitcoin", "Litecoin", "Dogecoin", "Avax", "Matic", "BNB", "Vite", "Digibyte", "Stellar lumen", "Solana"];
-let index = 0;
-setInterval(function() {
-    document.getElementById("crypto-img").src = logos[index];
-    document.getElementById("crypto-name").innerHTML = cryptos[index];
-    index = (index + 1) % logos.length;
-}, 2000);
+document.addEventListener("DOMContentLoaded", function() {
+    let logos = ["./IMG/CryptoLogo/ethereum.webp", "./IMG/CryptoLogo/bitcoin.webp", "./IMG/CryptoLogo/litecion.webp", "./IMG/CryptoLogo/dogecoin.webp", "./IMG/CryptoLogo/avax.webp", "./IMG/CryptoLogo/matic.webp", "./IMG/CryptoLogo/Bnb.webp", "./IMG/CryptoLogo/Vite.png", "./IMG/CryptoLogo/digibyte.webp", "./IMG/CryptoLogo/stellar.webp", "./IMG/CryptoLogo/solana.webp"];
+    let cryptos = ["Ethereum", "Bitcoin", "Litecoin", "Dogecoin", "Avax", "Matic", "BNB", "Vite", "Digibyte", "Stellar lumen", "Solana"];
+    let index = 0;
+
+    const cryptoContainer = document.querySelector(".crypto-container");
+
+    function changeCrypto() {
+        cryptoContainer.style.transform = "translateX(-100%)"; // Slide out to the left
+        cryptoContainer.style.opacity = 0; // Fade out
+
+        setTimeout(function() {
+            document.getElementById("crypto-img").src = logos[index];
+            document.getElementById("crypto-name").innerHTML = cryptos[index];
+
+            cryptoContainer.style.transform = "translateX(0)"; // Slide in from the left
+            cryptoContainer.style.opacity = 1; // Fade in
+
+            index = (index + 1) % logos.length;
+        }, 500); // Adjust this timeout to control the timing of the animation
+    }
+
+    setInterval(changeCrypto, 2000);
+    changeCrypto(); // Call the function to display the initial crypto
+});
+
+
